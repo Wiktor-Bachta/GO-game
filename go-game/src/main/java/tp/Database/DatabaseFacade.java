c94b7c88f98983056c7010a15230a6bd98bf4b28
Wiktor <wiktor.bachta@gmail.com>
2024-01-27 02:03:31 +0100

    database config

--- go-game/src/main/java/tp/Database/DatabaseFacade.java
+++ go-game/src/main/java/tp/Database/DatabaseFacade.java
@@ -1,5 +1,8 @@
 package tp.Database;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -36,7 +39,12 @@ public class DatabaseFacade {
     }
 
     public void open() {
-        emf = Persistence.createEntityManagerFactory("default");
+        Map<String, String> properties = new HashMap<String, String>();
+        properties.put("javax.persistence.jdbc.user", System.getenv("GoGameHistoryDataBaseUser"));
+        System.out.println(System.getenv("GoGameHistoryDataBaseUser"));
+        properties.put("javax.persistence.jdbc.password", System.getenv("GoGameHistoryDataBasePasswor"));
+
+        emf = Persistence.createEntityManagerFactory("default", properties);
     }
 
     public void close() {
